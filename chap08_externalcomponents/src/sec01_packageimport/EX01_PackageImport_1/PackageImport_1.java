9ecdeade58ae1efd61787dc0f4d3c1dce237690f
kkooHan1124 <iaw941224@gmail.com>
2022-09-26 17:18:54 +0900

    chap 7 & 8  & 9

---
+++ chap08_externalcomponents/src/sec01_packageimport/EX01_PackageImport_1/PackageImport_1.java
@@ -0,0 +1,15 @@
+package sec01_packageimport.EX01_PackageImport_1;
+
+public class PackageImport_1 {
+	public static void main(String[] args) {
+		// 객체 생성
+		// A a = new A();   // 오류
+		sec01_packageimport.common.A a = new sec01_packageimport.common.A ();
+		
+		// 멤버 활용
+		System.out.println(a.m);
+		System.out.println(a.n);
+		a.print();
+	}
+
+}
