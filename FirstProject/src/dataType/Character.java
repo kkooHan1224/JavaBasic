820196d210d98dc59bd6e7917b1b8a5bf59dd736
d <iaw941224@gmail.com>
2022-11-24 13:43:38 +0900

    22 11 24 자료형

---
+++ FirstProject/src/dataType/Character.java
@@ -0,0 +1,11 @@
+package dataType;
+
+public class Character {
+	public static void main(String[] args) {
+		char ch1 = '각';
+		char ch2 = '\uac01';
+		String ch3 = "서정한";
+		System.out.println(ch1);
+		System.out.println(ch2);
+	}
+}
---
+++ FirstProject/src/dataType/Hello.java
@@ -0,0 +1,9 @@
+package dataType;
+
+public class Hello {
+	public static void main(String[] args) {
+		System.out.print("안녕하세요. 여러분 저는 서정한입니다!");		
+		System.out.println("\n안녕하세요. \n여러분 \n저는 \n서정한입니다!");		
+		System.out.print("안녕하세요. 여러분 저는 서정한입니다!");		
+	}
+}
