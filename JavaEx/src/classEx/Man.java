6811cd0663a5631061d6393c5063ab9fb0003297
d <iaw941224@gmail.com>
2022-12-06 14:04:45 +0900

    22 12 06 JAVA 필드, 메서드

---
+++ JavaEx/src/classEx/Man.java
@@ -0,0 +1,33 @@
+package classEx;
+
+public class Man {
+	String name;
+	int age;
+	boolean Married;
+	int child;
+	
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public int getAge() {
+		return age;
+	}
+	public void setAge(int age) {
+		this.age = age;
+	}
+	public boolean isMarried() {
+		return Married;
+	}
+	public void setMarried(boolean married) {
+		Married = married;
+	}
+	public int getChild() {
+		return child;
+	}
+	public void setChild(int child) {
+		this.child = child;
+	}	
+}
